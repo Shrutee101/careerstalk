10fdac25189149b0ed1693bbb7cb8c862ed77903
shrutee151 <shrutee151@gmail.com>
2023-05-03 19:00:27 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Chat from './pages/Chat';
+import Index from './pages/Index';
+import {Routes,Route} from 'react-router-dom'
+import Talk from './pages/Talk';
+import Live from './pages/Live';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   
+    <Routes>
+      <Route path="/" element={<><Index/></>}/>
+      <Route path="/chat" element={<><Chat/></>}/>
+      <Route path='/talk' element={<><Talk/></>}/>
+      <Route path='/live' element={<><Live/></>}/>
+      <Route path='/blog' element={<><Blog/></>}/>
+      <Route path='/contact' element={<><Contact/></>}/>
+    </Routes>
+    </>
   );
 }
 
